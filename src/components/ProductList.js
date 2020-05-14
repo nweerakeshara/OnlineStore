28fb4e386ca4b60ac424851bc6891ba270ed74b7
Kumarage K. I. B it18100280 <it18100280@my.sliit.lk>
2020-05-14 21:29:13 +0530

    Added ProductList

---
+++ src/components/ProductList.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function ProductList() {
+  const productsData = [
+    {
+      name: "Red Tshirt",
+      price: 10.99,
+      id: 1,
+    },
+    {
+      name: "Blue Tshirt",
+      price: 24.99,
+      id: 1,
+    },
+    {
+      name: "Yellow Tshirt",
+      price: 18.99,
+      id: 1,
+    },
+  ];
+
+  return (
+    <div>
+      <h1>List</h1>
+    </div>
+  );
+}
--- src/components/user.component.js
+++ src/components/user.component.js
@@ -1,11 +1,13 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import ProductList from "./ProductList";
 
-export default class User extends Component{
-    render(){
-        return(
-            <div>
-                <h3 align="center"> This is User page </h3>
-            </div>
-        )
-    }
+export default class User extends Component {
+  render() {
+    return (
+      <div>
+        <h3 align="center"> This is User page </h3>
+        <ProductList />
+      </div>
+    );
+  }
 }
