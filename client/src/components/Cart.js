1b9836d13855b6b8cecf199b01f3538ea52b6e7d
Kumarage K. I. B it18100280 <it18100280@my.sliit.lk>
2020-05-17 19:07:02 +0530

    Moved cart button

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -19,7 +19,11 @@ export default function Cart({ buttonLabel }) {
 
   return (
     <div>
-      <Button color="danger" onClick={toggle}>
+      <Button
+        style={{ float: "right", margin: "20px" }}
+        color="danger"
+        onClick={toggle}
+      >
         {`${buttonLabel} ${cart.length}`}
       </Button>
       <Modal size="lg" isOpen={modal} toggle={toggle}>
