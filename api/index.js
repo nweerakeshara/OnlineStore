48330e5c9609fb0ecb92bde4ff0407250308b8aa
hrathnapala <43751413+hrathnapala@users.noreply.github.com>
2020-05-19 20:36:50 +0530

    wish list added

--- api/index.js
+++ api/index.js
@@ -17,6 +17,7 @@ app.use('/api/cus', require('./routes/cus'));
 app.use('/api/items', require('./routes/shoppingItems'));
 app.use('/product', require('./routes/product.route'));
 app.use('/api/storemanager', require('./routes/storemanager.route'));
+app.use('/api/wishlist',require('./routes/wishlist.route'));
 
 
 app.get('/', (req,res) =>{
---
+++ client/src/components/WishList.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function WishList({ name, price, id }) {
+  const [wishList, setWishList] = useState([]);
+
+  const addToWishList = (e) => {
+    const product = {
+      product_name: name,
+      product_price: price,
+      product_id: id,
+    };
+
+    axios
+      .post("http://localhost:5000/api/wishlist/add", product)
+      .then((res) => {
+       
+        alert("successfully added");
+        console.log(res.data);
+       
+      }).catch( () => {
+        console.log('eer');
+        
+      })
+        
+  };
+
+  return (
+    <div>
+      <button onClick={addToWishList} type="button" class="btn btn-secondary">
+        Add to Wish List
+      </button>
+    </div>
+  );
+}
