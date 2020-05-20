4c0d0707d93ba5779819c337b0b91fa9f53f4dd3
hrathnapala <43751413+hrathnapala@users.noreply.github.com>
2020-05-20 12:04:46 +0530

    functions completed

--- client/src/components/WishList.js
+++ client/src/components/WishList.js
@@ -1,8 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "react-notifications/lib/notifications.css";
-import {NotificationContainer, NotificationManager} from 'react-notifications';
-
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
+import WishListView from "./WishList.view";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Table,
+} from "reactstrap";
 
 export default function WishList({ name, price, id }) {
   const addToWishList = (e) => {
@@ -16,21 +27,58 @@ export default function WishList({ name, price, id }) {
       .post("http://localhost:5000/api/wishlist/add", product)
       .then((res) => {
         if (res.data.success == true) {
-          NotificationManager.success('Item Added to the Wish List', '');
+          NotificationManager.success("Click Here to view the Wish List", "Item Added to the Wish List",10000,()=>{
+            toggle();
+          });
           console.log(res.data);
         } else {
-          NotificationManager.error('Item is already in the Wish List', 'Click Here!', 10000, () => {
-              alert('Visit Wish List')
-          });
+          NotificationManager.error(
+            "",
+            "Item is already in the Wish List",
+            10000,
+            () => {
+              toggle()
+            }
+          );
 
           console.log(res.data);
         }
       })
       .catch((error) => {
-        NotificationManager.error('Item is already in the Wish List', 'Click Here!', 10000, () => {
-          alert('Visit Wish List')
-        });
+        NotificationManager.error(
+          "Click Here to view the Wish List!",
+          "Item is already in the Wish List",
+          10000,
+          () => {
+            toggle();
+          }
+        );
       });
+  };
+
+  const [wishlist, setWishList] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/wishlist/get").then((res) => {
+      setWishList(res.data);
+    });
+  });
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  const deleteItem = (e) => {
+  //  wishlist.filter(item => item.person_id !== e.target.value).map(filteredItems => (
+  //     setWishList(filteredItems)
+  //  ));
+    axios
+      .delete(`http://localhost:5000/api/wishlist/delete/${e.target.value}`)
+      .then((res) => {
+        NotificationManager.info('Item Successfully deleted',"",2000);
+        console.log(res.data);
+      })
+      .catch((err) => console.log("Error"));
   };
 
   return (
@@ -38,7 +86,53 @@ export default function WishList({ name, price, id }) {
       <button onClick={addToWishList} type="button" class="btn btn-secondary">
         Add to Wish List
       </button>
-      <NotificationContainer/>
+      <NotificationContainer />
+      <Modal size="lg" isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Your Wish List</ModalHeader>
+        {wishlist.length === 0 ? (
+          <ModalBody>
+            <p>No Items in Wish List</p>
+          </ModalBody>
+        ) : (
+          <ModalBody>
+            <Table>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Item</th>
+                  <th>Price</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              {wishlist.map((item) => (
+                <tbody key={item.product_id}>
+                  <tr>
+                    <th scope="row">{item.product_id}</th>
+                    <td>{item.product_name}</td>
+                    <td>{item.product_price}</td>
+                    <td>
+                      <button
+                        value={item.product_id}
+                        onClick={deleteItem}
+                        type="button"
+                        class="btn btn-danger"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              ))}
+            </Table>
+          </ModalBody>
+        )}
+
+        <ModalFooter>
+          <Button color="secondary" onClick={toggle}>
+            Continue Shopping
+          </Button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
