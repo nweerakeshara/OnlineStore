e9366bda725427a9476f91270a313d4f3343817c
Thanuja Chamika <thanujachamikatc@gmail.com>
2020-05-22 01:16:52 +0530

    Updated admin page

--- client/src/components/AdminPage/admin2.component.js
+++ client/src/components/AdminPage/admin2.component.js
@@ -1,10 +1,4 @@
 import React, {Component} from "react";
-import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
-import Guest from "../GuestPage/guest.component";
-import User from "../UserPage/user.component";
-import FullTable from "../StoreManagerPage/fullTable.component";
-import AddProduct from "../StoreManagerPage/addProduct.component";
-import EditProduct from "../StoreManagerPage/editProduct.component";
 import ButtonRow from "./buttonrow.component";
 
 export default class Admin2 extends Component{
